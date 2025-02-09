150c4f3c6d0310688b086930b694e74224c01774
dxgbj3 <daler.zh10@mail.ru>
2025-02-09 18:38:32 +0500

    first commit

--- main.py
+++ main.py
@@ -1,49 +1,68 @@
+# Импортирование библеотеки pygame
 import pygame, sys
+# Импортирование классов, функций с других файлов
 from scripts.utils import load_image, load_images
 from scripts.entities import PhysicsEntity
 from scripts.tilemap import Tilemap
+from scripts.clouds import Clouds
 
-class Game:
+class Game: # создание основного классе
     def __init__(self):
-        pygame.init()
+        pygame.init() # Инициализация
 
-        pygame.display.set_caption("Platformer")
-        self.screen = pygame.display.set_mode((640, 480))
-        self.display = pygame.Surface((320, 240))
+        pygame.display.set_caption("Platformer") # Название окна
+        self.screen = pygame.display.set_mode((640, 480)) # Создание дисплея игры
+        self.display = pygame.Surface((320, 240)) # Основной дисплей благодаря которого мы увеличиваем картинку в двое.
 
-        self.clock = pygame.time.Clock()
+        self.clock = pygame.time.Clock() # Clock позволяет иметь фиксированный фпс для более точной работы физики
 
         self.movement = [False, False]
 
+        # Выгрузка спрайтов
         self.assets = {
             'decor': load_images('tiles/decor'),
             'grass': load_images('tiles/grass'),
             'large_decor': load_images('tiles/large_decor'),
             'stone': load_images('tiles/stone'),
             'player': load_image('entities/player.png'),
+            'background': load_image('background.png'),
+            'clouds': load_images('clouds'),
         }
 
-        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
+        self.clouds = Clouds(self.assets['clouds'], count=16) # Облака
 
-        self.tilemap = Tilemap(self, tile_size=16)
+        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15)) # Игрок
+
+        self.tilemap = Tilemap(self, tile_size=16) # Карта (лвл)
+        self.tilemap.load('map.json')
+
+        self.scroll = [0, 0] # камера
 
     def run(self):
         while True:
-            self.display.fill((14, 219, 248))
+            self.display.blit(self.assets['background'], (0, 0))
 
-            self.tilemap.render(self.display)
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 25 # Применяем "физику" камеры. Значение в конце функции означает "плавность движения камеры"
+            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 25 # Чем дальше игрок от камеры, тем интенсивнее она следует за игроком
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
+            self.clouds.update() # Облака
+            self.clouds.render(self.display, offset=render_scroll)
+
+            self.tilemap.render(self.display, offset=render_scroll)
 
 
-           # print(self.tilemap.physics_rects_around(self.player.pos))
+           # print(self.tilemap.physics_rects_around(self.player.pos)) # Отображение коллизий в терминале
 
-
+            # Вызов функций движения и рендер игрока
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
-            self.player.render(self.display)
-
+            self.player.render(self.display, offset=render_scroll)
+            # Обязательный цикл для работы экрана и закрытия экрана
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                # Проверка нажатий клавиш клавиатуры
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_a:
                         self.movement[0] = True
@@ -57,7 +76,7 @@ class Game:
                     if event.key == pygame.K_d:
                         self.movement[1] = False
 
-            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0)) # Увеличение экрана без потерь
             pygame.display.update()
             self.clock.tick(60)
 
--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -1,3 +1,5 @@
+import json
+
 import pygame
 
 NEIGHBOUR_OFFSET = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (0, 0), (-1, 1), (0, 1), (1, 1)]
@@ -10,9 +12,9 @@ class Tilemap:
         self.tilemap = {}
         self.offgrid_tiles = []
 
-        for i in range(10):
-            self.tilemap[str(3 + i) + ';10'] = {'type': 'grass', 'variant': 1, 'pos': (3 + i, 10)}
-            self.tilemap['10;' + str(5 + i)] = {'type': 'stone', 'variant': 1, 'pos': (10, 5 + i)}
+        # for i in range(10):
+        #     self.tilemap[str(3 + i) + ';10'] = {'type': 'grass', 'variant': 1, 'pos': (3 + i, 10)}
+        #     self.tilemap['10;' + str(5 + i)] = {'type': 'stone', 'variant': 1, 'pos': (10, 5 + i)}
 
     def tiles_around(self, pos):
         tiles = []
@@ -23,6 +25,15 @@ class Tilemap:
                 tiles.append(self.tilemap[check_loc])
         return tiles
 
+    def load(self, path):
+        f = open(path, 'r')
+        map_data = json.load(f)
+        f.close()
+
+        self.tilemap = map_data['tilemap']
+        self.tile_size = map_data['tile_size']
+        self.offgrid_tiles = map_data['offgrid']
+
     def physics_rects_around(self, pos):
         rects = []
         for tile in self.tiles_around(pos):
@@ -30,10 +41,10 @@ class Tilemap:
                 rects.append(pygame.Rect(tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size, self.tile_size, self.tile_size))
         return rects
 
-    def render(self, surf):
-        for tile in self.offgrid_tiles:
-            surf.blit(self.game.assets[tile['type']][tile['variant']], tile['pos'])
+    def render(self, surf, offset=(0, 0)):
+        for tile in self.offgrid_tiles: # Отображение спрайтов которые не находятся на основной сетке спрайтов
+            surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] - offset[0], tile['pos'][1] - offset[1])) # рендер изображения
 
-        for loc in self.tilemap:
+        for loc in self.tilemap: # Спрайты загружены в словарь, и с помощью этого цикла мы достаём нужный нам ключ и значение
             tile = self.tilemap[loc]
-            surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size))
+            surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_size - offset[0], tile['pos'][1] * self.tile_size - offset[1]))
