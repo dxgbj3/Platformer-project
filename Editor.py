dbc3e097d89f32e6461c863f0af3e82923d133e8
dxgbj3 <daler.zh10@mail.ru>
2025-02-16 15:07:32 +0500

    Editor added and you can change stuff in it

--- Editor.py
+++ Editor.py
@@ -28,24 +28,38 @@ class Editor: # создание основного классе
         }
 
         self.tilemap = Tilemap(self, tile_size=16) # Карта (лвл)
-        self.tilemap.load('map.json')
 
         self.scroll = [0, 0] # камера
 
-        self.tile_list = list(self.assets) # Даст лист с выгруженными ассетами
+        # Исключаем background из списка тайлов
+        self.tile_list = [key for key in self.assets if key != "background"]
         self.tile_group = 0
         self.tile_variant = 0
         self.click = False
         self.r_click = False
+        self.shift = False
 
     def run(self):
         while True:
             self.display.blit(self.assets['background'], (0, 0))
 
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
+            self.tilemap.render(self.display, offset=render_scroll)
+
             current_tile_img = self.assets[self.tile_list[self.tile_group]][self.tile_variant].copy()
-            current_tile_img.set_alpha(100) # Прозрачность выбранной фигуры
+            current_tile_img.set_alpha(100) # Прозрачность выбранной фигуры от 0 до 255
 
+            mpos = pygame.mouse.get_pos()
+            mpos = (mpos[0] / RENDER_SCALE, mpos[1] / RENDER_SCALE) # Это нужно, чтобы получить правильные координаты мышки поскольку я использую мудрёную систему увеличения всего экрана
+            tile_pos = (int((mpos[0] + self.scroll[0]) // self.tilemap.tile_size), int((mpos[1] + self.scroll[1]) // self.tilemap.tile_size))
 
+            if self.click:
+                self.tilemap.tilemap[str(tile_pos[0]) + ';' + str(tile_pos[1])] = {'type': self.tile_list[self.tile_group], 'variant': self.tile_variant, 'pos': tile_pos}
+            if self.r_click:
+                tile_loc = str(tile_pos[0]) + ';' + str(tile_pos[1])
+                if tile_loc in self.tilemap.tilemap:
+                    del self.tilemap.tilemap[tile_loc]
 
             self.display.blit(current_tile_img, (5, 5))
 
@@ -60,10 +74,25 @@ class Editor: # создание основного классе
                         self.click = True
                     if event.button == 3: # Нажатие ПКМ
                         self.r_click = True
-                    if event.button == 4:
-                        self.tile_group = (self.tile_group - 1) % len(self.assets[self.tile_list[self.tile_group]]) # Смена тайлов на колёсико мыши. % len позволяет сделать цикл чтобы не уйти за пределы вариантов
-                    if event.button == 5:
-                        self.tile_group = (self.tile_group + 1) % len(self.assets[self.tile_list[self.tile_group]])
+                    if self.shift:
+                        if event.button == 4:  # Прокрутка вверх (смена варианта)
+                            self.tile_variant = (self.tile_variant - 1) % len(
+                                self.assets[self.tile_list[self.tile_group]])
+                        if event.button == 5:  # Прокрутка вниз (смена варианта)
+                            self.tile_variant = (self.tile_variant + 1) % len(
+                                self.assets[self.tile_list[self.tile_group]])
+                    else:
+                        if event.button == 4:  # Прокрутка вверх (смена группы)
+                            self.tile_group = (self.tile_group - 1) % len(self.tile_list)
+                            self.tile_variant = 0  # Сброс варианта при смене группы
+                        if event.button == 5:  # Прокрутка вниз (смена группы)
+                            self.tile_group = (self.tile_group + 1) % len(self.tile_list)
+                            self.tile_variant = 0  # Сброс варианта при смене группы
+                if event.type == pygame.MOUSEBUTTONUP:
+                    if event.button == 1:
+                        self.click = False
+                    if event.button == 3:
+                        self.r_click = False
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_a:
                         self.movement[0] = True
@@ -73,6 +102,8 @@ class Editor: # создание основного классе
                         self.movement[2] = True
                     if event.key == pygame.K_s:
                         self.movement[3] = True
+                    if event.key == pygame.K_LSHIFT:
+                        self.shift = True
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_a:
                         self.movement[0] = False
@@ -82,6 +113,8 @@ class Editor: # создание основного классе
                         self.movement[2] = False
                     if event.key == pygame.K_s:
                         self.movement[3] = False
+                    if event.key == pygame.K_LSHIFT:
+                        self.shift = False
 
             self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0)) # Увеличение экрана без потерь
             pygame.display.update()
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Platformer-project-
 My personal project, probably gonna be in my academy project showcase
+
+if you don't use this venv you have paste this into a terminal
+
+pip install pygame-ce
+
+pip install pygame_gui
